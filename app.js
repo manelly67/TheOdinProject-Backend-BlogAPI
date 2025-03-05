79cbb2c4dca2403f54d7a592f20208f1ff6fb160
manelly67 <118464447+manelly67@users.noreply.github.com>
2025-03-04 19:02:25 -0500

    configure cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const routes = require("./routes");
 
 const app = express();
 // Enable All CORS Requests
-app.use(cors());
+app.use(cors({origin:"http://localhost:5173/",allowedHeaders:['Content-Type', 'Authorization','Connection'],credentials:true}));
 
 app.use(
   session({
